dabc67b58aba23da29880c5fd0ae4c9ecff95276
Your Name <you@example.com>
2025-09-05 15:54:35 -0600

    missing AppContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AppProvider } from './context/AppContext.jsx'
+import { AppProvider } from '../context/AppContext.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
