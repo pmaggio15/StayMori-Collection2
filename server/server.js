1bc5ac83c80ddd5b9cf82624ce3a2de4007843e8
Your Name <you@example.com>
2025-09-04 22:01:53 -0600

    room routes backend

--- server/server.js
+++ server/server.js
@@ -4,15 +4,15 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebHooks.js";
-// import userRouter from "./routes/userRoutes.js";
-// import hotelRouter from "./routes/hotelRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
 // import roomRouter from "./routes/roomRoutes.js";
 // import bookingRouter from "./routes/bookingRoutes.js";
-// import connectCloudinary from "./configs/cloudinary.js";
+import connectCloudinary from "./configs/cloudinary.js";
 // import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
 
 connectDB();
-// connectCloudinary();
+connectCloudinary();
 
 const app = express();
 app.use(cors()); 
@@ -28,8 +28,8 @@ app.use(clerkMiddleware());
 app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is working"));
-// app.use("/api/user", userRouter);
-// app.use("/api/hotels", hotelRouter);
+app.use("/api/user", userRouter);
+app.use("/api/hotels", hotelRouter);
 // app.use("/api/rooms", roomRouter);
 // app.use("/api/bookings", bookingRouter);
 
